dd339dcce8ad05521e54488d4319a8b515f4668d
Tabakov Mikhail Aleksandrovich Samsung <mat13@mail.ru>
2022-06-30 16:30:28 +0500

    SAMSUNG

--- Excplorer/Expl.cs
+++ Excplorer/Expl.cs
@@ -45,7 +45,7 @@ namespace Explorer
       {
          InitializeComponent();
 
-         
+         // SAMSUNG
          // Проверка всех мониторов и возможности открыть окно по сохранённым координатам
          Screen[] aryScreen;
          aryScreen = Screen.AllScreens;

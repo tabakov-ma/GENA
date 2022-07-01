6d0c0972b8608ae0595956f02779537369746555
Tabakov Mikhail Aleksandrovich <mat13@mail.ru>
2022-07-01 13:02:43 +0500

    ASUS 7

--- Excplorer/Expl.cs
+++ Excplorer/Expl.cs
@@ -45,7 +45,7 @@ namespace Explorer
       {
          InitializeComponent();
 
-         // ASUS 6
+         // ASUS 7
          // Проверка всех мониторов и возможности открыть окно по сохранённым координатам
          Screen[] aryScreen;
          aryScreen = Screen.AllScreens;

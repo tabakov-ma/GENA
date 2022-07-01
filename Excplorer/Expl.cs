042367e20e2e837baf03ce4604e0c490a5f909f2
Tabakov Mikhail Aleksandrovich <mat13@mail.ru>
2022-07-01 10:12:18 +0500

    ASUS 6

--- Excplorer/Expl.cs
+++ Excplorer/Expl.cs
@@ -45,7 +45,7 @@ namespace Explorer
       {
          InitializeComponent();
 
-         // SAMSUNG 5
+         // ASUS 6
          // Проверка всех мониторов и возможности открыть окно по сохранённым координатам
          Screen[] aryScreen;
          aryScreen = Screen.AllScreens;
